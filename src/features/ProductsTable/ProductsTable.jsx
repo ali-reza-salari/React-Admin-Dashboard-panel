78494d3ed6559fc38788419416dab47d2dc48822
Ali Reza Salari <asalari.dev853@gmail.com>
2026-09-21 09:32:43 -0700

    fix: refactor search logick for products table

--- src/features/ProductsTable/ProductsTable.jsx
+++ src/features/ProductsTable/ProductsTable.jsx
@@ -8,13 +8,20 @@ import productsTableHeadCellRows from "../../data/productsTableHeadCellRows.js";
 import productsData from "../../data/productsData.js";
 import {IoIosMore} from "react-icons/io";
 import {IoIosSearch} from "react-icons/io";
-import {useEffect, useState} from "react";
+import {startTransition, useEffect, useState} from "react";
 
 export default function ProductsTable({  }) {
-    const [search , setSearch] = useState("")
-    const searchedProduct = productsData.filter(product => (
-        product.product_title.toLowerCase().includes(search.toLowerCase())
-    ))
+    const [filteredProducts , setFilteredProducts] = useState(productsData);
+    const [searchResult , setSearchResult] = useState("")
+    const handelSearch = (event) => {
+        const searchInputValue = event.target.value;
+        setSearchResult(searchInputValue)
+
+        startTransition(() => {
+            const filterProducts = productsData.filter(product => product.product_title.toLowerCase().includes(searchInputValue.toLowerCase()))
+            setFilteredProducts(filterProducts)
+        })
+    }
 
     return (
         <div className={`overflow-x-auto`}>
@@ -25,8 +32,8 @@ export default function ProductsTable({  }) {
                     <input
                         className="w-full rounded-xl border border-slate-200 bg-transparent py-2.5 pl-10 pr-4 text-sm outline-none focus:border-brand-500 dark:border-slate-700"
                         placeholder="Search products..."
-                        value={search}
-                        onChange={e => setSearch(e.target.value)}
+                        value={searchResult}
+                        onChange={handelSearch}
                     />
                 </label>
 
@@ -59,7 +66,7 @@ export default function ProductsTable({  }) {
 
                 <TableBody>
                     {
-                        searchedProduct.map(product => (
+                        filteredProducts.map(product => (
                             <TableRow key={product.id}>
                                 <TableCell>
                                     <div className="px-2 py-4">
@@ -108,60 +115,6 @@ export default function ProductsTable({  }) {
                         ))
                     }
                 </TableBody>
-
-
-
-                {/*<TableBody>*/}
-                {/*    {*/}
-                {/*        products.map(product => (*/}
-                {/*            <TableRow key={product.id}>*/}
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-2 py-4">*/}
-                {/*                        <div className="flex items-center gap-3">*/}
-                {/*                            <div className="h-11 w-11 rounded-lg bg-stone-200 dark:bg-stone-700">*/}
-                {/*                                <img src={product.product_image} />*/}
-                {/*                            </div>*/}
-                {/*                            <div>*/}
-                {/*                                <p className="font-semibold">{product.product_title}</p>*/}
-                {/*                                <p className="mt-0.5 text-xs text-slate-500">{product.product_description}</p>*/}
-                {/*                            </div>*/}
-                {/*                        </div>*/}
-                {/*                    </div>*/}
-                {/*                </TableCell>*/}
-
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-5 py-4 text-slate-500">{product.product_brand}</div>*/}
-                {/*                </TableCell>*/}
-
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-5 py-4">*/}
-                {/*                        <span className="font-semibold">{product.product_stock}</span>*/}
-                {/*                    </div>*/}
-                {/*                </TableCell>*/}
-
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-5 py-4 font-semibold">${product.product_price.toLocaleString()}</div>*/}
-                {/*                </TableCell>*/}
-
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-5 py-4">*/}
-                {/*                        <span className={`rounded-full bg-emerald-50 px-2.5 py-1 text-xs font-semibold dark:bg-emerald-500/15 ${product.product_stock === 0 ? "dark:text-yellow-300 text-yellow-700" : "dark:text-emerald-300 text-emerald-700"}`}>*/}
-                {/*                            {product.product_stock === 0 ? "Low Stock" : "Active"}*/}
-                {/*                        </span>*/}
-                {/*                    </div>*/}
-                {/*                </TableCell>*/}
-
-                {/*                <TableCell>*/}
-                {/*                    <div className="px-5 py-4">*/}
-                {/*                        <button className="text-slate-400 hover:text-brand-600 cursor-pointer" aria-label="Product options">*/}
-                {/*                            <IoIosMore className={"size-6"} />*/}
-                {/*                        </button>*/}
-                {/*                    </div>*/}
-                {/*                </TableCell>*/}
-                {/*            </TableRow>*/}
-                {/*        ))*/}
-                {/*    }*/}
-                {/*</TableBody>*/}
             </Table>
         </div>
     )
